a82107d727dd59bd497b6144272bb404203711b0
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-11-28 16:27:16 +0100

    getAllNurses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetAllNursesComponent } from './get-all-nurses/get-all-nurses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetAllNursesComponent
   ],
   imports: [
     BrowserModule,
