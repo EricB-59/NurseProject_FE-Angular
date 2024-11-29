b2c9a5103623657b1b6f1b833f9c954dbf413250
EricB-59 <ebaenac59@gmail.com>
2024-11-29 16:39:22 +0100

    app_routing - fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GetAllNursesComponent } from './get-all-nurses/get-all-nurses.component';
 import { SearchByNameNursesComponent } from './search-by-name-nurses/search-by-name-nurses.component';
+import { LoginNursesComponent } from './login-nurses/login-nurses.component';
 
 const routes: Routes = [
   { path: 'getAll', component: GetAllNursesComponent },
-  { path: 'login', component: GetAllNursesComponent },
+  { path: 'login', component: LoginNursesComponent },
   { path: 'searchByName', component: SearchByNameNursesComponent },
 ];
 
