477e4aaf97e90ba4e9ee19ef645050012a085b24
EricB-59 <ebaenac59@gmail.com>
2024-11-29 15:48:53 +0100

    nav_route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { GetAllNursesComponent } from './get-all-nurses/get-all-nurses.component';
 import { SearchByNameNursesComponent } from './search-by-name-nurses/search-by-name-nurses.component';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideRouter } from '@angular/router';
 
 @NgModule({
   declarations: [
