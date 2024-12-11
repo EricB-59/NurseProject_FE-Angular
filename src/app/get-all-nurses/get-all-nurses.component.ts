c4ef1def88d3c9412e0c2800d2f004ef6f25e626
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-12-11 15:34:40 +0100

    first commit

--- src/app/get-all-nurses/get-all-nurses.component.ts
+++ src/app/get-all-nurses/get-all-nurses.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import jsonData from '../data/DATA.json';
+import { NurseService } from '../nurses/nurse.service';
 @Component({
   selector: 'app-get-all-nurses',
   templateUrl: './get-all-nurses.component.html',
-  styleUrl: './get-all-nurses.component.css'
+  styleUrl: './get-all-nurses.component.css',
+  providers: [NurseService]
 })
-export class GetAllNursesComponent {
+export class GetAllNursesComponent implements OnInit {
   nurses: any[] = jsonData; 
+  constructor(private nurseService: NurseService) {}
+  ngOnInit(): void {
+    this.nurseService.getAllNurses();
+    console.log(this.nurseService.getAllNurses());
+  }
 
 }
--- src/app/nurses/nurse.service.ts
+++ src/app/nurses/nurse.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import jsonData from '../data/DATA.json';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NurseService {
-  constructor() { }
 
   nurses: any[] = jsonData;
 
   getAllNurses() {
-
+    return "hola";
   }
 
   registerNurse() {
