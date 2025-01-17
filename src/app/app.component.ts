905ca67fb733a5881d497919dfdcf5d672caf3b1
Guillem6969 <guim.san.oli@gmail.com>
2025-01-17 16:20:08 +0100

    #38 GET ALL solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,8 @@ import { NurseService } from './nurses/nurse.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   title = 'NurseProject_FE-Angular';
   logged: boolean = false;
 
-  constructor(private _userService: NurseService) {}
-
-  ngOnInit() {
-    this._userService.getAllNurses().subscribe({
-      next: (data) => console.log('User data:', data),
-      error: (err) => console.error('Error:', err),
-    });
-  }
 }
