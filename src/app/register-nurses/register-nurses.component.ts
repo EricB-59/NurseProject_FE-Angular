32ac94343c3605506ef1ef975cdcb7bda90eceb6
EricB-59 <ebaenac59@gmail.com>
2025-01-31 15:36:58 +0100

    Profile picture FE implementation

--- src/app/register-nurses/register-nurses.component.ts
+++ src/app/register-nurses/register-nurses.component.ts
@@ -12,12 +12,14 @@ import { Nurse } from '../nurse';
 })
 export class RegisterNursesComponent {
   existe: boolean = false;
+  selectedFileBase64: string | null = null;
   constructor(private router: Router, private _userService: NurseService) {}
 
   form = new FormGroup({
     first_name: new FormControl(''),
     last_name: new FormControl(''),
     email: new FormControl(''),
+    profileImg: new FormControl(''),
     password: new FormControl(''),
     repeat_password: new FormControl(''),
   });
@@ -28,6 +30,7 @@ export class RegisterNursesComponent {
         first_name: this.form.value.first_name as string,
         last_name: this.form.value.last_name as string,
         email: this.form.value.email as string,
+        profileImg: this.selectedFileBase64 as string,
         password: this.form.value.password as string,
       });
       this._userService.registerNurse(nurse).subscribe((result) => {
@@ -35,5 +38,24 @@ export class RegisterNursesComponent {
       });
       this.router.navigate(['/login']);
     }
+  }
+
+  onFileChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.convertFileToBase64(file);
+    }
+  }
+
+  convertFileToBase64(file: File): void {
+    const reader = new FileReader();
+    reader.readAsDataURL(file); // Convierte el archivo a base64
+    reader.onload = () => {
+      this.selectedFileBase64 = reader.result as string;
+      console.log('Archivo convertido a base64:', this.selectedFileBase64);
+    };
+    reader.onerror = (error) => {
+      console.error('Error al leer el archivo:', error);
+    };
   }
 }
